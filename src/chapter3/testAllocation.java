396ef7e90a65c6ef5cd823de32ecdb2ed86fe156
zhaoshq <523618033@qq.com>
2017-07-29 12:58:48 +0800

    新生代Minor GC测试，以及对象经过分配担保机制进入老年代

--- src/chapter3/testAllocation.java
+++ src/chapter3/testAllocation.java
@@ -10,15 +10,35 @@ public class testAllocation {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
-        byte[] allocation1,allocation2,allocation3,allocation4;
+        byte[] allocation1,allocation2,allocation3,allocation4,allocation5,allocation6,allocation7;
         allocation1 = new byte[ 2 * _1MB];
         allocation2 = new byte[ 2 * _1MB];
         allocation3 = new byte[ 2 * _1MB];
-        //出现一次Minor GC，这是因为给allocation4分配所需的4M内存时，发现Eden已经被占用6MB，剩余空间已经不足以分配allocation4的4MB内存，所以发生MinorGC。不过，因为每个对象都有引用，所以这次GC总内存占用量计划没有减少（7471K->6675K）
-        //GC期间JVM发现已有的3个2MB大小的对象全部无法放入Survivor空间（Survivor空间只有1MB大小）
-        //所以只好通过担保机制提前转移到老年代了
-        allocation4 = new byte[ 4 * _1MB];
-        //这次GC结束后，4MB的allocation4对象顺利分配到Eden中，（eden space 8192K,  53% used）
-        //因此程序执行玩的结果是Eden占用4MB，Survivor空闲，老年代被占用6MB（被allocation1，allocation2，allocation3占用，total 10240K, used 6144K），通过GC日志可以看到
+        allocation1 = null;
+        /*
+        * 出现一次Minor GC，这是因为给allocation4分配所需的2M内存时，发现Eden已经被占用7.8MB（6MB对象，其他为程序所需空间），
+        * 剩余空间已经不足以分配allocation4的2MB内存，所以发生MinorGC。
+        * 不过，因为allocation2，allocation3都有引用，所以这次GC总内存占用量减少了allocation1的2M（7645K->4714K）
+        * GC期间JVM发现已有的2个2MB大小的对象全部无法放入Survivor空间（Survivor空间只有1MB大小）,
+        * 所以只好通过担保机制提前转移到老年代了（tenured generation   total 10240K, used 4096K ）
+        * */
+        allocation4 = new byte[ 2 * _1MB];
+        allocation5 = new byte[ 2 * _1MB];
+        allocation6 = new byte[ 2 * _1MB];
+        allocation5 = null;
+        allocation6 = null;
+        allocation2 = null;
+        /*
+        * 这里再出现一次Minor GC，出现的原因同上。
+        * allocation5，allocation6被清空，
+        * allocation4无法放入Survior区，所以通过担保机制进入老年代，此时新生代为空（7004K->1K(9216K)）
+        * 虽然老年代中的allocation2已经失效，但是因为老年代的GC没有触发，
+        * 所以老年代被占用6MB(tenured generation   total 10240K, used 6760K)
+        *
+        * */
+        allocation7 = new byte[ 2 * _1MB];
+        allocation7 = null;
+        //经过这次System.gc()，老年代将把失效的allocation2空间删除，整理后为4MB(Tenured: 6763K->4710K(10240K))
+        System.gc();
     }
 }
